6f9d5766ef67cc5c0cee42777fe345da44089902
uc02 <upendrachauhan0206@gmail.com>
2025-06-01 15:31:17 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Required for static deployments
-  images: {
-    unoptimized: true, // Disable Image Optimization (static exports don't support it)
-    remotePatterns: [
+  /* config options here */
+  images:{
+    remotePatterns:[
       {
         protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
-  // Optional: Add basePath if deploying to GitHub Pages
-  // basePath: '/your-repo-name',
+        hostname:"images.pexels.com",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
